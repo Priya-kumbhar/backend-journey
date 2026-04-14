9d5b50e46ac937c37b3e237700f0ac9df4424a89
Priyanka Kumbhar <priyankakumbhar.2405@gmail.com>
2026-04-14 12:54:38 +0530

    started new project

---
+++ Day-03/server.js
@@ -0,0 +1,12 @@
+//import express
+const express =require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    res.send("server is running..");
+});
+app.get('/about', (req, res) => {
+    res.send("This is About Page");
+});
+app.listen(3000,()=>{
+    console.log("server started on port 30000");
+});
